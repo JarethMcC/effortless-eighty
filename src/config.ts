2784c5e677c581081411f75ff57dccf4aed92e92
Jareth <jarethmccardell@gmail.com>
2025-05-23 21:19:42 +0100

    Updating API base URL

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Backend API base URL
-export const API_BASE_URL: string = 'https://jarethm.com/api';
+export const API_BASE_URL: string = 'https://strava-api-handler-jp7zta2ltq-uc.a.run.app/api';
 
 // Strava redirect URI - make sure this matches EXACTLY what's registered with Strava
 export const STRAVA_REDIRECT_URI: string = 'https://effortlesseighty.com/exchange_token';
