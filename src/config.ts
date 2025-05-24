f6d96a29630494d2d4eb5b82851d80b2ac41b16f
Jareth <jarethmccardell@gmail.com>
2025-05-24 21:46:34 +0100

    Reverting endpoint URLs

--- src/config.ts
+++ src/config.ts
@@ -5,11 +5,11 @@ export const API_BASE_URL: string = 'https://effortless-eighty.onrender.com';
 export const STRAVA_REDIRECT_URI: string = 'https://effortless-eighty-frontend.onrender.com/exchange_token';
 
 // API endpoints
-export const API_AUTH_URL: string = `${API_BASE_URL}/api_auth_url`;
-export const API_EXCHANGE_TOKEN: string = `${API_BASE_URL}/api_exchange_token`;
-export const API_REFRESH_TOKEN: string = `${API_BASE_URL}/api_refresh_token`;
-export const API_ACTIVITIES: string = `${API_BASE_URL}/api_activities`;
-export const API_ATHLETE_ZONES: string = `${API_BASE_URL}/api_athlete_zones`;
+export const API_AUTH_URL: string = `${API_BASE_URL}/auth-url`;
+export const API_EXCHANGE_TOKEN: string = `${API_BASE_URL}/exchange-token`;
+export const API_REFRESH_TOKEN: string = `${API_BASE_URL}/refresh-token`;
+export const API_ACTIVITIES: string = `${API_BASE_URL}/activities`;
+export const API_ATHLETE_ZONES: string = `${API_BASE_URL}/athlete/zones`;
 
 // Strava API scopes
 export const STRAVA_SCOPES: string = 'read,activity:read_all,profile:read_all';
